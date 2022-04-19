ef59d6ef677a4c0ce8c84dc939f618ba8e3f60b2
KP <kyleprock@verizon.net>
2022-04-19 15:55:48 -0500

    some changes

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,9 +1,11 @@
 // Handles anything related to the banner/top portion of this app
+import './top.css'
+
 const Top = () => {
     return(
         <div className="top">
             <h1>To Do List</h1>
-            <button id="add-task">Add Task</button>
+            <button id="add-task">+Add Task</button>
         </div>
     )
 }
