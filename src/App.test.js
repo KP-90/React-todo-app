715f429e8a66e4790b4b8903016b2883af5521f3
KP <kyleprock@verizon.net>
2022-04-22 11:59:54 -0500

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { getByRole, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import Top from './components/Top';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App renders", () => {
+  it("Renders App", () => {
+    const {container} = render(<App />)
+    expect(container).toMatchSnapshot()
+  })
+  it("Renders top section", () => {
+    const { getByRole } = render(<Top />)
+    expect(getByRole("heading").textContent).toMatch(/to do list/i)
+  })
+})
+
+describe("Users can add task", () => {
+  beforeEach(() => {
+    const {container} = render(<App />)
+  })
+  it("Button show modal", () => {
+    const btn = screen.getByRole("button", {name: "+Add Task"})
+    userEvent.click(btn)
+    expect(screen.getByRole("modal").style.display).toMatch(/flex/i)
+
+  })
+})
