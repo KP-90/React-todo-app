2da23888a3bbac1ffcc2010b58807eacf0dc9dc1
KP <kyleprock@verizon.net>
2022-04-25 13:27:19 -0500

    fixed bug in testing

--- src/App.test.js
+++ src/App.test.js
@@ -16,7 +16,7 @@ describe("App renders", () => {
 
 describe("Users can add task", () => {
   beforeEach(() => {
-    const {container} = render(<App />)
+    let {container} = render(<App />)
   })
   it("Add task button shows modal", () => {
     const btn = screen.getByRole("button", {name: "+Add Task"})
@@ -24,20 +24,18 @@ describe("Users can add task", () => {
     expect(screen.getByRole("modal").style.display).toMatch(/flex/i)
   })
   it("Renders tasks", () => {
-    const tasks = screen.queryAllByRole("task")
+    const tasks = screen.queryAllByRole("task-container")
     expect(tasks.length).toBeGreaterThan(0)
   })
   it("Tasks can be added and rendered", async() => {
-    let userInput
-    const tasksBefore = screen.queryAllByRole("task")
+    const tasksBefore = screen.queryAllByRole("task-container")
     const btn = screen.getByRole("button", {name: "+Add Task"})
-    await  userEvent.click(btn)
-    console.log("MODAL IS VISIBLE")
-    userInput = screen.getByRole("inputText")
+    userEvent.click(btn)
+    let userInput = screen.getByRole("inputText")
     await userEvent.type(userInput, `test`)
     const submitBtn = screen.getByRole("close")
-    await userEvent.click(submitBtn)
-    const tasksAfter = screen.queryAllByRole("task")
+    userEvent.click(submitBtn)
+    const tasksAfter = screen.queryAllByRole("task-container")
     expect(tasksBefore.length).toBeLessThan(tasksAfter.length)
   })
 })
